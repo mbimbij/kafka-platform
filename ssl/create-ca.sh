8a0e69a78244e9ac1f800ce9b4a0c9ee856f1cb1
mbimbij <joseph.mbimbi@gmail.com>
2021-04-18 06:55:35 +0200

    Improve cert creation script

--- ssl/create-ca.sh
+++ ssl/create-ca.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 openssl req -x509 -config openssl-ca.cnf -newkey rsa:4096 -sha256 -nodes -out cacert.pem -outform PEM
+if [ ! -f serial.txt ]; then echo 01 > serial.txt;  else echo "serial.txt already present" ; fi
+if [ ! -f index.txt ]; then touch index.txt;  else echo "index.txt already present" ; fi
--- ssl/create-jks.sh
+++ ssl/create-jks.sh
@@ -1,36 +1,34 @@
 #!/bin/bash
 
-if [[ $# != 2 ]]; then
-  echo -e "usage:\n./create-keystore.sh \$ENTITY_NAME \$HOSTNAME"
+if [[ $# != 1 ]]; then
+  echo -e "usage:\n./create-keystore.sh \$ENTITY_NAME"
   exit 1
 fi
 
+ENTITY_NAME=$1
 PASSWORD=changeit
-CSR_NAME=$1.key.csr
-SIGNED_CSR_NAME=$1.key.signed.csr
-KEYSTORE_NAME=$1.keystore.jks
-TRUSTSTORE_NAME=$1.truststore.jks
-SUPPLEMENT_HOSTNAME=$2
+CSR_NAME=$ENTITY_NAME.key.csr
+SIGNED_CSR_NAME=$ENTITY_NAME.key.signed.csr
+KEYSTORE_NAME=$ENTITY_NAME.keystore.jks
+TRUSTSTORE_NAME=$ENTITY_NAME.truststore.jks
 
 echo "##################################################################################################"
 echo "# création du keystore du client"
 echo "##################################################################################################"
-keytool -keystore $1.keystore.jks \
-  -alias $1 \
+keytool -keystore $ENTITY_NAME.keystore.jks \
+  -alias $ENTITY_NAME \
   -validity 365 \
   -genkey -keyalg RSA -storetype pkcs12 -storepass $PASSWORD \
-  -dname "CN=$1, OU=TEST, O=SOME_ORG, L=SOME_PLACE, S=SOME_STATE, C=FR"
+  -dname "CN=$ENTITY_NAME, OU=TEST, O=SOME_ORG, L=SOME_PLACE, S=SOME_STATE, C=FR"
 
 echo "##################################################################################################"
 echo "# création d'un csr pour le client"
 echo "##################################################################################################"
-keytool -certreq -alias $1 -file $CSR_NAME -keystore $KEYSTORE_NAME -storepass $PASSWORD -ext SAN=DNS:localhost,DNS:$(hostname),DNS:$SUPPLEMENT_HOSTNAME,IP:127.0.0.1
+keytool -certreq -alias $ENTITY_NAME -file $CSR_NAME -keystore $KEYSTORE_NAME -storepass $PASSWORD -ext SAN=DNS:localhost,DNS:$(hostname),DNS:$ENTITY_NAME,IP:127.0.0.1
 
 echo "##################################################################################################"
 echo "# signature du csr du client par le root ca"
 echo "##################################################################################################"
-if [ ! -f serial.txt ]; then echo 01 > serial.txt;  else echo "serial.txt already present" ; fi
-if [ ! -f index.txt ]; then touch index.txt;  else echo "index.txt already present" ; fi
 openssl ca -batch -config openssl-ca.cnf -policy signing_policy -extensions signing_req -out $SIGNED_CSR_NAME -infiles $CSR_NAME
 
 echo "##################################################################################################"
@@ -42,7 +40,7 @@ echo "##########################################################################
 echo "# update du keystore du client avec le ca root et la clé signée"
 echo "##################################################################################################"
 keytool -keystore $KEYSTORE_NAME -alias caroot -import -file cacert.pem -noprompt -storepass $PASSWORD
-keytool -keystore $KEYSTORE_NAME -alias $1 -import -file $SIGNED_CSR_NAME -noprompt -storepass $PASSWORD
+keytool -keystore $KEYSTORE_NAME -alias $ENTITY_NAME -import -file $SIGNED_CSR_NAME -noprompt -storepass $PASSWORD
 
 rm $CSR_NAME
 rm $SIGNED_CSR_NAME
