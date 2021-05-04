169cc505911ae8866055c9631f52b09128b68fb8
mbimbij <joseph.mbimbi@gmail.com>
2021-05-04 10:24:13 +0200

    Differentiate between application name and its directory, for infra and pipeline creation

--- create-all.sh
+++ create-all.sh
@@ -13,7 +13,7 @@ source infra/infra.env
 
 echo "##############################################################################"
 echo "creating networking: VPC, etc."
-echo -e "##############################################################################\n\n"
+echo -e "##############################################################################\n"
 # create vpc and networking
 aws cloudformation deploy \
   --stack-name $NETWORKING_STACK_NAME \
@@ -22,37 +22,41 @@ aws cloudformation deploy \
 
 echo "##############################################################################"
 echo "creating cicd pipeline for topic CRUD action platform"
-echo -e "##############################################################################\n\n"
+echo -e "##############################################################################\n"
 # create cicd pipeline for topics crud actions
 aws cloudformation deploy    \
   --stack-name $APPLICATION_NAME-topic-actions-pipeline   \
   --template-file topic-actions/pipeline-stack.yml    \
   --capabilities CAPABILITY_NAMED_IAM   \
-  --parameter-overrides     ApplicationName=$APPLICATION_NAME-topic-actions     \
-  GithubRepo=$GITHUB_REPO     \
-  SamStackName=$APPLICATION_NAME-topic-actions-sam-stack
+  --parameter-overrides     \
+    ApplicationName=$APPLICATION_NAME-topic-actions     \
+    ApplicationDirectoryName=topic-actions \
+    GithubRepo=$GITHUB_REPO     \
+    SamStackName=$APPLICATION_NAME-topic-actions-sam-stack
 
 echo "##############################################################################"
 echo "creating cicd pipeline for lambda authorizer platform"
-echo -e "##############################################################################\n\n"
+echo -e "##############################################################################\n"
 # create cicd pipeline for topics crud actions
 aws cloudformation deploy    \
   --stack-name $APPLICATION_NAME-lambda-authorizer-pipeline   \
   --template-file lambda-authorizer/pipeline-stack.yml    \
   --capabilities CAPABILITY_NAMED_IAM   \
-  --parameter-overrides     ApplicationName=$APPLICATION_NAME-lambda-authorizer     \
-  GithubRepo=$GITHUB_REPO     \
-  SamStackName=$APPLICATION_NAME-lambda-authorizer-sam-stack
+  --parameter-overrides     \
+    ApplicationName=$APPLICATION_NAME-lambda-authorizer     \
+    ApplicationDirectoryName=lambda-authorizer \
+    GithubRepo=$GITHUB_REPO     \
+    SamStackName=$APPLICATION_NAME-lambda-authorizer-sam-stack
 
 echo "##############################################################################"
 echo "creating kafka cluster"
-echo -e "##############################################################################\n\n"
+echo -e "##############################################################################\n"
 # create kafka cluster
 infra/kafka/create-kafka-cluster.sh
 
 echo "##############################################################################"
 echo "kafka cluster, kafka topic CRUD actions platform pipeline, lambda authorizer pipeline creation done"
-echo -e "##############################################################################\n\n"
+echo -e "##############################################################################\n"
 
 #echo "##############################################################################"
 #echo "creating kafka topic 'test'"
