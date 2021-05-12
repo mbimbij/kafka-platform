e358106f255598f3a1ef7ce62b499f032d91074b
mbimbij <joseph.mbimbi@gmail.com>
2021-05-12 10:59:03 +0200

    Add more logs to delete-all.sh script

--- delete-all.sh
+++ delete-all.sh
@@ -10,20 +10,29 @@ KAFKA_STACK_NAME=$APPLICATION_NAME-kafka
 NETWORKING_STACK_NAME=$APPLICATION_NAME-network
 source infra/infra.env
 
-echo "##############################################################################"
-echo "Deleting lambda authorizer pipeline and sam stack"
+echo -e "##############################################################################"
+echo -e "Deleting sam stack"
 echo -e "##############################################################################\n"
 aws cloudformation delete-stack --stack-name $APPLICATION_NAME-topic-actions-sam-stack
 aws cloudformation wait stack-delete-complete --stack-name $APPLICATION_NAME-topic-actions-sam-stack
 
+echo -e "##############################################################################"
+echo -e "Deleting pipeline"
+echo -e "##############################################################################\n"
 ./scripts/empty-s3-bucket.sh $AWS_REGION-$ACCOUNT_ID-$APPLICATION_NAME-topic-actions-pipeline-bucket
 aws cloudformation delete-stack --stack-name $APPLICATION_NAME-topic-actions-pipeline
 aws cloudformation wait stack-delete-complete --stack-name $APPLICATION_NAME-topic-actions-pipeline
 
+echo -e "##############################################################################"
+echo -e "Deleting kafka cluster"
+echo -e "##############################################################################\n"
 # delete kafka stack
 aws cloudformation delete-stack --stack-name $KAFKA_STACK_NAME
 aws cloudformation wait stack-delete-complete --stack-name $KAFKA_STACK_NAME
 
+echo -e "##############################################################################"
+echo -e "Deleting network"
+echo -e "##############################################################################\n"
 # delete networking stack
 aws cloudformation delete-stack --stack-name $NETWORKING_STACK_NAME
 aws cloudformation wait stack-delete-complete --stack-name $NETWORKING_STACK_NAME
